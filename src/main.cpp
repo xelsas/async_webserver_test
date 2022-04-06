18654a90ff9f2cdfe86d74a75f198e9e09802dbb
Xander Elsas <xelsas@gmail.com>
2022-04-06 03:37:14 +0200

    added code to control an led matrix

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,13 @@
 #include <ESPAsyncTCP.h>
 #endif
 #include <ESPAsyncWebServer.h>
+#include <MD_MAX72xx.h>
+#include <MD_Parola.h>
+#include <SPI.h>
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 8
+#define CS_PIN 5
 
 // WiFi credentials
 const char* ssid     = "";     
@@ -14,6 +21,12 @@ const char* password = "";
 
 // WebServer https://github.com/me-no-dev/ESPAsyncWebServer
 AsyncWebServer server(80);
+
+// https://registry.platformio.org/libraries/majicdesigns/MD_MAX72XX
+// https://registry.platformio.org/libraries/majicdesigns/MD_Parola
+MD_Parola led_marquee = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+String led_marquee_text = "scrolling text";
+bool led_marquee_text_changed = true;
 
 String config_html = "<!DOCTYPE html>"
                      "<html>"
@@ -41,9 +54,11 @@ void handleConfigPostRequest(AsyncWebServerRequest *request) {
   Serial.print("\n");
   if (request->hasParam("data", true, false)) {
     AsyncWebParameter* p_data = request->getParam("data", true, false);
+
     Serial.printf("Received data: %s\n", p_data->value().c_str());
-  } else {
-    Serial.println("Received no data.");
+
+    led_marquee_text = p_data->value();
+    led_marquee_text_changed = true;
   }
 
   request->send(200, "text/html", config_html);
@@ -87,6 +102,20 @@ void setup() {
   
   // Start web server
   server.begin();
+
+  led_marquee.begin();
+  led_marquee.setIntensity(0);
+  led_marquee.displayClear();
 }
 
-void loop() {}
+void loop() {
+  if (led_marquee_text_changed) {
+    led_marquee_text_changed = false;
+    led_marquee.displayClear();
+    led_marquee.displayText(led_marquee_text.c_str(), PA_CENTER, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  }
+
+  if (led_marquee.displayAnimate()) {
+    led_marquee.displayReset();
+  }
+}
