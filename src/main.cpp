ed8c29bf354810760310afc314c8960645a81e34
Xander Elsas <xelsas@gmail.com>
2022-04-06 11:34:53 +0200

    If it's going to be ESP32 specific anyway, might as well remove generic code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include <Arduino.h>
-#ifdef ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#endif
 #include <ESPAsyncWebServer.h>
 #include <MD_MAX72xx.h>
 #include <MD_Parola.h>
