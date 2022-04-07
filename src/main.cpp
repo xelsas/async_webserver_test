2a34b17903edcd0ae0584609dfd7a3cce557ac0c
Xander Elsas <xelsas@gmail.com>
2022-04-08 00:40:37 +0200

    start in access point mode if a wifi connection cannot be made

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,39 @@
 #define CS_PIN 5
 
 // WiFi credentials
-const char* ssid     = "";     
+const char* ssid     = "";
 const char* password = "";
+const char* ap_ssid     = "ESP32";
+const char* ap_password = "1234567890";
+
+bool restart_wifi = false;
+
+String wifi_config_html = "<!DOCTYPE html>"
+                     "<html>"
+                     "<head>"
+                     "</head>"
+                     "<body>"
+                     " <h1>ESP32 Web Server</h1>"
+                     " <form method='post' action='/wifi'>"
+                     "  <label>SSID<input type='text' name='ssid' value='' /></label><br>"
+                     "  <label>Password<input type='password' name='password' value='' /></label><br>"
+                     "  <input type='submit' value='submit' />"
+                     " </form>"
+                     "</body>"
+                     "</html>";
+
+String config_html = "<!DOCTYPE html>"
+                     "<html>"
+                     "<head>"
+                     "</head>"
+                     "<body>"
+                     " <h1>ESP32 Web Server</h1>"
+                     " <form method='post' action='/'>"
+                     "  <input type='text' name='data' value='' /><br>"
+                     "  <input type='submit' value='submit' />"
+                     " </form>"
+                     "</body>"
+                     "</html>";
 
 // WebServer https://github.com/me-no-dev/ESPAsyncWebServer
 AsyncWebServer server(80);
@@ -26,18 +57,26 @@ MD_Parola led_marquee = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 String led_marquee_text = "scrolling text";
 bool led_marquee_text_changed = true;
 
-String config_html = "<!DOCTYPE html>"
-                     "<html>"
-                     "<head>"
-                     "</head>"
-                     "<body>"
-                     " <h1>ESP32 Web Server</h1>"
-                     " <form method='post' action='/'>"
-                     "  <input type='text' name='data' value='' />"
-                     "  <input type='submit' value='submit' />"
-                     " </form>"
-                     "</body>"
-                     "</html>";
+/**
+ * Function to handle the wifi configuration page post requests
+ * 
+ * @param AsyncWebServerRequest *request
+ */
+void handleWiFiConfigPostRequest(AsyncWebServerRequest *request) {
+  if (request->hasParam("ssid", true, false) && request->hasParam("password", true, false)) {
+    AsyncWebParameter* p_ssid = request->getParam("ssid", true, false);
+    AsyncWebParameter* p_password = request->getParam("password", true, false);
+
+    Serial.printf("Received SSID: %s\n", p_ssid->value().c_str());
+
+    ssid     = p_ssid->value().c_str();
+    password = p_password->value().c_str();
+    
+    restart_wifi = true;
+  }
+
+  request->send(200, "text/html", config_html);
+}
 
 /**
  * Function to handle any 404 response
@@ -49,17 +88,11 @@ void handleNotFound(AsyncWebServerRequest *request) {
 }
 
 /**
- * Function to the configuration page post requests
+ * Function to handle the configuration page post requests
  * 
  * @param AsyncWebServerRequest *request
  */
 void handleConfigPostRequest(AsyncWebServerRequest *request) {
-  int headers = request->headers();
-  for(int i=0;i<headers;i++){
-    Serial.printf("%s: %s\n", request->headerName(i).c_str(), request->header(i).c_str());
-  }
-
-  Serial.print("\n");
   if (request->hasParam("data", true, false)) {
     AsyncWebParameter* p_data = request->getParam("data", true, false);
 
@@ -77,6 +110,7 @@ void handleConfigPostRequest(AsyncWebServerRequest *request) {
 /**
  * Setup the web server routing:
  * / root for the configuration page
+ * /wifi for the wifi configuration page
  * any other path should respond with a 404
  */
 void setupWebServerRouting() {
@@ -94,6 +128,53 @@ void setupWebServerRouting() {
     HTTP_POST,
     handleConfigPostRequest
   );
+
+  // Setup the /wifi get handler
+  server.on(
+    "/wifi",
+    HTTP_GET,
+    [](AsyncWebServerRequest *request){
+      request->send(200, "text/html", wifi_config_html);
+    }
+  );
+  // Setup the /wifi post handler
+  server.on(
+    "/wifi",
+    HTTP_POST,
+    handleWiFiConfigPostRequest
+  );
+
+  server.onNotFound(handleNotFound);
+}
+
+/**
+ * Handle WiFi setup, try to connect and fall back to starting as access point.
+ * 
+ * Default AP ip is 192.168.4.1
+ */
+void setupWiFi() {
+  WiFi.disconnect();
+  WiFi.softAPdisconnect();
+  
+  int count = 0;
+  WiFi.begin(ssid, password);             
+  while (WiFi.status() != WL_CONNECTED && count < 20) { 
+    delay(500);
+    Serial.print('.');
+    count++;
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Configuring access point.");
+    WiFi.softAP(ap_ssid, ap_password);
+
+    return;
+  }
+  
+  Serial.println('\n');
+  Serial.println("Connected with IP address:\t");
+  Serial.println(WiFi.localIP());
+  Serial.println('\n');
 }
 
 /**
@@ -103,18 +184,8 @@ void setup() {
   Serial.begin(115200);         
   delay(10);
   Serial.println('\n');
-  
-  // Handle WiFi setup
-  WiFi.begin(ssid, password);             
-  while (WiFi.status() != WL_CONNECTED) 
-  { 
-    delay(500);
-    Serial.print('.');
-  }
-  Serial.println('\n');
-  Serial.println("Connected with IP address:\t");
-  Serial.println(WiFi.localIP());
-  Serial.println('\n');
+
+  setupWiFi();
 
   setupWebServerRouting();
   
@@ -131,6 +202,13 @@ void setup() {
  * The main loop handles updating the led matrix, and changes the text when needed.
  */
 void loop() {
+  if (restart_wifi) {
+    restart_wifi = false;
+
+    setupWiFi();
+  }
+  
+
   if (led_marquee_text_changed) {
     led_marquee_text_changed = false;
     led_marquee.displayClear();
